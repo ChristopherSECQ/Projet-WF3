56d34640a742a11decf7af9f5f098bc0fe227ba1
ChristopherSECQ <christophersecq@gmail.com>
2018-11-18 20:19:05 +0100

    header+nav

--- accueil.php
+++ accueil.php
@@ -10,13 +10,13 @@
 
     <body>
 
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
 
-                    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <nav class="navbar navbar-expand-lg navbar-dark bg-dark d-flex justify-content-between">
 
-                        <a class="navbar-brand" href="#">Projet WF3</a>
+                        <a class="navbar-brand" href="#">Le Vide-Grenier de Chris</a>
                         
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
@@ -28,18 +28,35 @@
                                     <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
                                 </li>
 
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">À propos de nous</a>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Vêtements
+                                    </a>
+
+                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <li><a href="#">Petite Fille</a></li>
+                                        <li><a href="#">Homme</a></li>
+                                    </ul>
                                 </li>
 
                                 
-
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Contacts</a>
+                                    <a class="nav-link" href="#">Jouets</a>
+                                </li>
+
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Pokémon
+                                    </a>
+
+                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <li><a href="#">Jouets</a></li>
+                                        <li><a href="#">Figurines</a></li>
+                                        <li><a href="#">Peluches</a></li>
+                                        <li><a href="#">Cartes</a></li>
+                                    </ul>
                                 </li>
                             </ul>
-
-                            
                         </div>
                     </nav>
 
@@ -51,5 +68,17 @@
         </div>
         <!-- /.container -->
         
+
+
+
+
+        <!-- JavaScript Bootstrap-->
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+        <!-- Jquery et Popper Bootstrap-->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+
     </body>
 </html>
